4199741aac79e825f1aed64a428a9d8f0a99366f
guille <guille@ejemplo.es>
2024-05-30 17:49:03 +0200

    Relacion one to one con fotos// Revisar Services muy importante!!

--- src/main/java/iesvdm/org/fighthubrestapi/service/UserService.java
+++ src/main/java/iesvdm/org/fighthubrestapi/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
                         + confirmationUrl)
                 .link(confirmationUrl)
                 .name(user.getUserName())
-                .build();
+               .build();
 
         emailService.sendEmailHtml(emailRequest);
     }
